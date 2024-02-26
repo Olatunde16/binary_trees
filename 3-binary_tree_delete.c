374c7e6fc13aebdb11e36891a7012edffeb2d760
Olutunmibi Iseoluwa <olatundeolutunmibi@gmail.com>
2024-02-26 01:38:09 -0800

    Mandatory Task 3 done

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree.
+ *
+ * @tree: A pointer to the root node of the tree to delete.
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
+}
