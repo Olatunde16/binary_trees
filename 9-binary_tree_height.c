3b1e8bee385bfae84c4ee13a21c3c6ad4e771e91
Olutunmibi Iseoluwa <olatundeolutunmibi@gmail.com>
2024-02-26 02:29:23 -0800

    Mandatory Task 9 done

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ *
+ * @tree: A pointer to the root node of the tree to measure the height.
+ *
+ * Return: 0 if tree is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t left_height = 0, right_height = 0;
+
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+		return ((left_height > right_height) ? left_height : right_height);
+	}
+
+	return (0);
+}
