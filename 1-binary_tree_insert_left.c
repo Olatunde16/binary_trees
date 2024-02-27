81a3386728061ca357dd2921c5b9404ec23372e1
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-02-27 18:16:30 +0100

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
@@ -11,26 +10,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
+
 	if (parent == NULL)
-		return NULL;
+		return (NULL);
 
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
-		return NULL;
-
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->right = NULL;
+		return (NULL);
 
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
-	else
-		new_node->left = NULL;
-
 	parent->left = new_node;
 
-	return(new_node);
+	return (new_node);
 }
