398fc458d4e735a311c22b8e1fae1db875f64b10
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-02-27 18:19:35 +0100

    Update 4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,8 +10,8 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return(0);
+	if (node == NULL || node->left != NULL || node->right != NULL)
+		return (0);
 
-	return (node->left == NULL && node->right == NULL) ? 1 : 0;
+	return (1);
 }
