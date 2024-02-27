ed1856895db1fc1eea030c1baf0eeabdcbbacd55
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-02-27 18:22:53 +0100

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,17 +10,5 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return(0);
-
-	size_t depth = 0;
-	const binary_tree_t *current = tree;
-
-	while (current->parent != NULL) 
-	{
-		depth++;
-		current = current->parent;
-	}
-
-	return(depth);
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
