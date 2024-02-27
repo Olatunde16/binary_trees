45b423b1ac0a31a08bc9f6fdfc428bff4fbf7772
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-02-27 19:10:12 +0100

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,6 +3,7 @@
 
 /**
  * binary_tree_sibling - Finds the sibling of a node in a binary tree
+ *
  * @node: A pointer to the node to find the sibling of
  *
  * Return: A pointer to the sibling node, or NULL if no sibling is found
@@ -11,7 +12,6 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return NULL;
-
 	if (node->parent->left == node)
 		return(node->parent->right);
 
