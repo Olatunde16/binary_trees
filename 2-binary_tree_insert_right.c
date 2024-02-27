edbd780cfda8024aa2ff988daa5eff3ef80b17f3
Olutunmibi Iseoluwa Olatunde <125552420+Olatunde16@users.noreply.github.com>
2024-02-27 18:18:23 +0100

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,26 +11,21 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_node;
+
 	if (parent == NULL)
-		return NULL;
+		return (NULL);
 
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
-		return NULL;
-
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
+		return (NULL);
 
 	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
 		parent->right->parent = new_node;
 	}
-	else
-		new_node->right = NULL;
-
 	parent->right = new_node;
 
-	return(new_node);
+	return (new_node);
 }
